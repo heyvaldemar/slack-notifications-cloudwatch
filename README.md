8af0476e849b39dff3da7554f401127d07f5663c
Vladimir Mikhalev <services@devops.ac>
2022-12-24 21:53:15 -0500

    Slack Notifications for CloudWatch

---
+++ README.md
@@ -0,0 +1,26 @@
+# Slack Notifications for CloudWatch
+
+Python script that helps post alarms from CloudWatch to Slack.
+
+# Author
+hey, I’m Vladimir Mikhalev, but my friends call me Valdemar.
+
+🌐 My [website](https://www.heyvaldemar.com/) with detailed IT guides\
+🎬 Follow me on [YouTube](https://www.youtube.com/channel/UCf85kQ0u1sYTTTyKVpxrlyQ?sub_confirmation=1)\
+🐦 Follow me on [Twitter](https://twitter.com/heyValdemar)\
+🎨 Follow me on [Instagram](https://www.instagram.com/heyvaldemar/)\
+🎸 Follow me on [Facebook](https://www.facebook.com/heyValdemarFB/)\
+🎥 Follow me on [TikTok](https://www.tiktok.com/@heyvaldemar)\
+💻 Follow me on [LinkedIn](https://www.linkedin.com/in/heyvaldemar/)\
+🐈 Follow me on [GitHub](https://github.com/heyvaldemar)
+
+# Communication
+👾 Chat with IT pros on [Discord](https://discord.gg/AJQGCCBcqf)\
+📧 Reach me at ask@sre.gg
+
+# Give Thanks
+💎 Support on [GitHub](https://github.com/sponsors/heyValdemar)\
+🏆 Support on [Patreon](https://www.patreon.com/heyValdemar)\
+🥤 Support on [BuyMeaCoffee](https://www.buymeacoffee.com/heyValdemar)\
+🍪 Support on [Ko-fi](https://ko-fi.com/heyValdemar)\
+💖 Support on [PayPal](https://www.paypal.com/paypalme/heyValdemarCOM)
---
+++ slack-notifications-cloudwatch.py
@@ -0,0 +1,178 @@
+# Slack Notifications for CloudWatch
+
+# Author
+# hey, I’m Vladimir Mikhalev, but my friends call me Valdemar.
+
+# My website with detailed IT guides: https://www.heyvaldemar.com/
+# Follow me on YouTube: https://www.youtube.com/channel/UCf85kQ0u1sYTTTyKVpxrlyQ?sub_confirmation=1
+# Follow me on Twitter: https://twitter.com/heyValdemar
+# Follow me on Instagram: https://www.instagram.com/heyvaldemar/
+# Follow me on Facebook: https://www.facebook.com/heyValdemarFB/
+# Follow me on TikTok: https://www.tiktok.com/@heyvaldemar
+# Follow me on [LinkedIn](https://www.linkedin.com/in/heyvaldemar/)
+# Follow me on [GitHub](https://github.com/heyvaldemar)
+
+# Communication
+# Chat with IT pros on Discord: https://discord.gg/AJQGCCBcqf
+# Reach me at ask@sre.gg
+
+# Give Thanks
+# Support on GitHub: https://github.com/sponsors/heyValdemar
+# Support on Patreon: https://www.patreon.com/heyValdemar
+# Support on BuyMeaCoffee: https://www.buymeacoffee.com/heyValdemar
+# Support on Ko-fi: https://ko-fi.com/heyValdemar
+# Support on PayPal: https://www.paypal.com/paypalme/heyValdemarCOM
+
+import urllib3
+import json
+
+# Slack incoming URL (replace with yours)
+slack_url = "https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
+http = urllib3.PoolManager()
+
+def get_alarm_attributes(sns_message):
+    alarm = dict()
+
+    alarm['name'] = sns_message['AlarmName']
+    alarm['description'] = sns_message['AlarmDescription']
+    alarm['reason'] = sns_message['NewStateReason']
+    alarm['region'] = sns_message['Region']
+    alarm['instance_id'] = sns_message['Trigger']['Dimensions'][0]['value']
+    alarm['state'] = sns_message['NewStateValue']
+    alarm['previous_state'] = sns_message['OldStateValue']
+
+    return alarm
+
+def register_alarm(alarm):
+    return {
+        "type": "home",
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":warning: " + alarm['name'] + " alarm was registered"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "_" + alarm['description'] + "_"
+                },
+                "block_id": "text1"
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Region: *" + alarm['region'] + "*"
+                    }
+                ]
+            }
+        ]
+    }
+
+def activate_alarm(alarm):
+    return {
+        "type": "home",
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":red_circle: Alarm: " + alarm['name'],
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "_" + alarm['reason'] + "_"
+                },
+                "block_id": "text1"
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Region: *" + alarm['region'] + "*"
+                    }
+                ]
+            }
+        ]
+    }
+
+def resolve_alarm(alarm):
+    return {
+        "type": "home",
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":large_green_circle: Alarm: " + alarm['name'] + " was resolved",
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "_" + alarm['reason'] + "_"
+                },
+                "block_id": "text1"
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "Region: *" + alarm['region'] + "*"
+                    }
+                ]
+            }
+        ]
+    }
+
+def lambda_handler(event, context):
+    sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
+    alarm = get_alarm_attributes(sns_message)
+
+    msg = str()
+
+    if alarm['previous_state'] == "INSUFFICIENT_DATA" and alarm['state'] == 'OK':
+        msg = register_alarm(alarm)
+    elif alarm['previous_state'] == 'OK' and alarm['state'] == 'ALARM':
+        msg = activate_alarm(alarm)
+    elif alarm['previous_state'] == 'ALARM' and alarm['state'] == 'OK':
+        msg = resolve_alarm(alarm)
+
+    encoded_msg = json.dumps(msg).encode("utf-8")
+    resp = http.request("POST", slack_url, body=encoded_msg)
+    print(
+        {
+            "message": msg,
+            "status_code": resp.status,
+            "response": resp.data,
+        }
+    )
